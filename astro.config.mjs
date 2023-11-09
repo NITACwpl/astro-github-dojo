9a9d98ba8cc510aa83bbe71695b62a9e1d3a601a
QwerTayu <tayu.sa99hire.make@gmail.com>
2023-11-09 12:11:24 +0900

    🏗️ github actionの設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [preact()],
+    buildOptions: {
+        site: "https://NITACwpl.github.io/",
+        base: "https://github.com/NITACwpl/astro-github-dojo"
+    },
+});
